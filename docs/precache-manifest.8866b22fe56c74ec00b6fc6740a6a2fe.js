14217eee2a0093629ec77b6aad2a26c31da32eee
carlosblade75 <carlosblade75@hotmail.com>
2020-08-02 19:39:08 +0200

    docs added

---
+++ docs/precache-manifest.8866b22fe56c74ec00b6fc6740a6a2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db9f89cb0358e788302ab1b42952352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a873df21edf1df5293be",
+    "url": "/static/css/main.d6125d2e.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a873df21edf1df5293be",
+    "url": "/static/js/main.6b53b503.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
